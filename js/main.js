8ab4923cf577dec6d0b0c5133ce7e11c37013e11
AnatolySoroka <toliksoroka66@gmail.com>
2022-09-02 23:31:27 +0300

    created all sections

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
+// Маска інпута
 let element = document.getElementById('phone');
 let maskOptions = {
     mask: '+(380)00-00-00-000',
     lazy: false
 }
 let mask = new IMask(element, maskOptions);
+
+// Кнопка бургер-меню
+let btn = document.querySelector('.burger__menu');
+let menu = document.querySelector('.menu');
+let body = document.querySelector('body');
+let links = document.querySelector('.menu__list-link');
+
+btn.addEventListener('click', function() {
+    menu.classList.toggle('active');
+    body.classList.toggle('active');
+});
+
+// Приховує меню при кліку на посилання
+links.addEventListener('click', function() {
+    menu.classList.remove('active');
+})
