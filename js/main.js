09691bff9e847385e99e411583bf1904ef7be513
AnatolySoroka <toliksoroka66@gmail.com>
2022-09-03 12:56:15 +0300

    adaptive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+$(function() {
+$('.burger__menu').click(function(event) {
+    $('.burger__menu,.menu').toggleClass('active');
+    $('body').toggleClass('lock');
+});
+})
+
+
 // Маска інпута
 let element = document.getElementById('phone');
 let maskOptions = {
@@ -7,17 +15,17 @@ let maskOptions = {
 let mask = new IMask(element, maskOptions);
 
 // Кнопка бургер-меню
-let btn = document.querySelector('.burger__menu');
-let menu = document.querySelector('.menu');
-let body = document.querySelector('body');
-let links = document.querySelector('.menu__list-link');
+// let btn = document.querySelector('.burger__menu');
+// let menu = document.querySelector('.menu');
+// let body = document.querySelector('body');
+// let links = document.querySelector('.menu__list-link');
 
-btn.addEventListener('click', function() {
-    menu.classList.toggle('active');
-    body.classList.toggle('active');
-});
+// btn.addEventListener('click', function() {
+//     menu.classList.toggle('active');
+//     body.classList.toggle('active');
+// });
 
-// Приховує меню при кліку на посилання
-links.addEventListener('click', function() {
-    menu.classList.remove('active');
-})
+// // Приховує меню при кліку на посилання
+// links.addEventListener('click', function() {
+//     menu.classList.remove('active');
+// })
