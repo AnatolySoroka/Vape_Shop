dc1ac38e888a4884723b0f93112650123fa7c667
AnatolySoroka <toliksoroka66@gmail.com>
2022-09-06 02:04:31 +0300

    added slider

--- js/main.js
+++ js/main.js
@@ -1,9 +1,41 @@
 $(function() {
-$('.burger__menu').click(function(event) {
-    $('.burger__menu,.menu').toggleClass('active');
-    $('body').toggleClass('lock');
-});
+    $('.burger__menu').click(function(event) {
+        $('.burger__menu,.menu').toggleClass('active');
+        $('html').toggleClass('lock');
+    });
+    $('form').submit(function(event) {
+        $('.popup').toggleClass('active');
+        $('html').toggleClass('lock');
+        return false
+    });
+    $('.close__btn').click(function(event) {
+        $('.popup').toggleClass('active');
+        $('html').toggleClass('lock');
+    })
+    $('.menu__list-link').click(function(event) {
+        $('.menu').toggleClass('active');
+        $('html').toggleClass('lock');
+    })
 })
+
+new Swiper('.swiper', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    // effect: 'coverflow',
+    // coverflowEffect: {
+    //     rotate: 0,
+    //     stretch: 1,
+    //     slideShadows: true,
+    // },
+    slidesPerView: 1.14,
+    // slideToClickedSlide: true,
+    loop: true,
+    spaceBetween: 15
+});
+
+
 
 
 // Маска інпута
