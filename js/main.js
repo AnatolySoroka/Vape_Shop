1a95c5c74cd115c756ec3a59a2bfceae94cf1499
AnatolySoroka <toliksoroka66@gmail.com>
2022-09-07 09:21:48 +0300

    edits

--- js/main.js
+++ js/main.js
@@ -58,19 +58,3 @@ let maskOptions = {
     lazy: false
 }
 let mask = new IMask(element, maskOptions);
-
-// Кнопка бургер-меню
-// let btn = document.querySelector('.burger__menu');
-// let menu = document.querySelector('.menu');
-// let body = document.querySelector('body');
-// let links = document.querySelector('.menu__list-link');
-
-// btn.addEventListener('click', function() {
-//     menu.classList.toggle('active');
-//     body.classList.toggle('active');
-// });
-
-// // Приховує меню при кліку на посилання
-// links.addEventListener('click', function() {
-//     menu.classList.remove('active');
-// })
